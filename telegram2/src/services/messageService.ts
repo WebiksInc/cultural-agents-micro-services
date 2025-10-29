63ea2402c1f3e6c9008e97990a99382e0027f953
yairb9 <yairb1997@gmail.com>
2025-10-29 12:40:24 +0200

    messageService was added

---
+++ telegram2/src/services/messageService.ts
@@ -0,0 +1,25 @@
+import { Api } from 'telegram';
+import * as sessionManager from './sessionManager';
+import * as validators from '../utils/validators';
+import * as logger from '../utils/logger';
+
+export const sendMessage = async (
+  fromPhone: string,
+  toTarget: string,
+  message: string
+): Promise<void> => {
+  validators.validatePhone(fromPhone);
+  validators.validateTarget(toTarget);
+  validators.validateMessage(message);
+
+  const client = sessionManager.getClient(fromPhone);
+  if (!client) {
+    throw new Error(`No authenticated session found for ${fromPhone}`);
+  }
+
+  logger.info('Sending message', { fromPhone, toTarget });
+
+  await client.sendMessage(toTarget, { message });
+
+  logger.info('Message sent successfully', { fromPhone, toTarget });
+};
