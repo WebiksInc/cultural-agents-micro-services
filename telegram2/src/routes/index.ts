09db27144a9b6eec6fc10b2e703b596816e5633b
yairb9 <yairb1997@gmail.com>
2025-11-09 17:51:21 +0200

    feat: add endpoint that returns the chat participants

--- telegram2/src/routes/index.ts
+++ telegram2/src/routes/index.ts
@@ -4,6 +4,7 @@ import messageRoutes from './messageRoutes';
 import unreadRoutes from './unreadRoutes';
 import chatsRoutes from './chatsRoutes';
 import chatMessagesRoutes from './chatMessagesRoutes';
+import chatParticipantsRoutes from './chatParticipantsRoutes';
 import pollRoutes from './pollRoutes';
 
 const router = Router();
@@ -13,6 +14,7 @@ router.use('/messages', messageRoutes);
 router.use('/messages', unreadRoutes);
 router.use('/chats', chatsRoutes);
 router.use('/chat-messages', chatMessagesRoutes);
+router.use('/chat-participants', chatParticipantsRoutes);
 router.use('/polls', pollRoutes);
 
 export default router;
