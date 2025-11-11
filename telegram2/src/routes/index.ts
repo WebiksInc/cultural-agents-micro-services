c259a15e6032153767885508a4047ff01885d6e4
yairb9 <yairb1997@gmail.com>
2025-11-11 14:28:56 +0200

    fixed docker

--- telegram2/src/routes/index.ts
+++ telegram2/src/routes/index.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import authRoutes from './authRoutes';
 import messageRoutes from './messageRoutes';
-import unreadRoutes from './unreadRoutes';
 import chatsRoutes from './chatsRoutes';
 import chatMessagesRoutes from './chatMessagesRoutes';
 import chatParticipantsRoutes from './chatParticipantsRoutes';
@@ -11,10 +10,9 @@ const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/messages', messageRoutes);
-router.use('/messages', unreadRoutes);
 router.use('/chats', chatsRoutes);
 router.use('/chat-messages', chatMessagesRoutes);
-router.use('/chat-participants', chatParticipantsRoutes);
+router.use('/participants', chatParticipantsRoutes);
 router.use('/polls', pollRoutes);
 
 export default router;
